fd62c134f90bd9e31f14cbda35ddcfbb0234a941
samel <samuelrigo@gmail.com>
2024-03-13 11:22:17 -0300

    Update button and input styles

--- src/components/text-field/text-field.component.tsx
+++ src/components/text-field/text-field.component.tsx
@@ -2,19 +2,20 @@ import Input from "@/components/input/input.component";
 
 interface TextFieldProps {
   label: string;
+  placeholder: string;
 }
 
 // object = {
 //   [text]: <Input />
 // [password]: <InputPassword />
 
-export default function TextField({label}: TextFieldProps) {
+export default function TextField({ label, placeholder }: TextFieldProps) {
   return (
     <>
-      <div>
-        <label></label>
-        <Input />
+      <div className="flex flex-col gap-1">
+        <label className="text-sm">{label}</label>
+        <Input placeholder={placeholder} />
       </div>
     </>
-  )
+  );
 }
