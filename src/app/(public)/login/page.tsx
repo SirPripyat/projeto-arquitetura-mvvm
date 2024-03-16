9e19b76d8c15aed2c1bd7331abd9072e0550f4b2
leonardorossi <08.leo.rossi@gmail.com>
2024-03-15 23:22:13 -0300

    feat: Melhorando page de login e

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,32 +1,30 @@
-import { Button } from "@/components/button";
-import { ButtonHierarchy } from "@/enum/button-hierarchy.enum";
-import TextField from "@/components/text-field/text-field.component";
-import Image from "next/image";
-import LoginUserForm from "./components/login-user-form.component";
+'use client';
+
+import { Button } from '@/components/button';
+import { ButtonHierarchy } from '@/enum/button-hierarchy.enum';
+import LoginUserForm from './components/login-user-form.component';
+import { useRouter } from 'next/navigation';
+import { Logo } from '@/components/logo';
 
 export default function Home() {
+  const { push } = useRouter();
+
   return (
-    <>
-      <Image
-        src="/background.svg"
-        alt="background"
-        className="absolute left-1 w-1/4 opacity-30"
-        width={100}
-        height={100}
-      />
+    <div className="flex flex-col justify-center gap-8 w-1/4">
+      <Logo.Medium />
 
-      <div className="flex flex-col justify-center gap-8 w-1/4">
-        <h1 className={"text-4xl text-gray-50 text-center"}>Project Name</h1>
+      <LoginUserForm />
 
-        <LoginUserForm />
-
-        <div className="flex flex-col justify-center items-center gap-1 text-gray-50 w-full">
-          <p>Não possui uma conta?</p>
-          <Button.Wrapper type={"button"} hierarchy={ButtonHierarchy.Ghosted}>
-            Criar Conta
-          </Button.Wrapper>
-        </div>
+      <div className="flex flex-col justify-center items-center gap-1 text-gray-50 w-full">
+        <p>Não possui uma conta?</p>
+        <Button.Wrapper
+          type={'button'}
+          hierarchy={ButtonHierarchy.Ghosted}
+          onClick={() => push('/register')}
+        >
+          Criar Conta
+        </Button.Wrapper>
       </div>
-    </>
+    </div>
   );
 }
