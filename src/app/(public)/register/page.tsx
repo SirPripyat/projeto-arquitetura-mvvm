d4ab55b39f88690f207e356e5b65170142eb9924
leonardorossi <08.leo.rossi@gmail.com>
2024-03-15 23:18:26 -0300

    feat: add novos components para o logo e formatando o arquivo

--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -1,32 +1,31 @@
-import { Button } from "@/components/button";
-import { ButtonHierarchy } from "@/enum/button-hierarchy.enum";
-import React from "react";
-import Image from "next/image";
-import RegisterUserForm from "@/app/(public)/register/components/register-user-form.component";
+'use client';
+
+import { Button } from '@/components/button';
+import { ButtonHierarchy } from '@/enum/button-hierarchy.enum';
+import React from 'react';
+import RegisterUserForm from '@/app/(public)/register/components/register-user-form.component';
+import { useRouter } from 'next/navigation';
+import { Logo } from '@/components/logo';
 
 export default function Register() {
+  const { push } = useRouter();
+
   return (
-    <>
-      <Image
-        src="/background.svg"
-        alt="background"
-        className="absolute left-1 w-1/4 opacity-30"
-        width={100}
-        height={100}
-      />
+    <div className="flex flex-col justify-center gap-8 w-1/4">
+      <Logo.Medium />
 
-      <div className="flex flex-col justify-center gap-8 w-1/4">
-        <h1 className={"text-4xl text-gray-50 text-center"}>Project Name</h1>
+      <RegisterUserForm />
 
-        <RegisterUserForm />
-
-        <div className="flex flex-col justify-center items-center gap-1 text-gray-50 w-full">
-          <p>Já possui conta?</p>
-          <Button.Wrapper type={"button"} hierarchy={ButtonHierarchy.Ghosted}>
-            Logar
-          </Button.Wrapper>
-        </div>
+      <div className="flex flex-col justify-center items-center gap-1 text-gray-50 w-full">
+        <p>Já possui conta?</p>
+        <Button.Wrapper
+          onClick={() => push('/login')}
+          type={'button'}
+          hierarchy={ButtonHierarchy.Ghosted}
+        >
+          Logar
+        </Button.Wrapper>
       </div>
-    </>
+    </div>
   );
 }
