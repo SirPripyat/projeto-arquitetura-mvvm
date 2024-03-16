ea3a4faace3bf49cda2799d42c62d90936f99d13
leonardorossi <08.leo.rossi@gmail.com>
2024-03-15 23:23:07 -0300

    feat: Lógica para fazer navegação entre as telas através do click do button

--- src/app/(public)/login/components/login-user-form.component.tsx
+++ src/app/(public)/login/components/login-user-form.component.tsx
@@ -1,13 +1,16 @@
-"use client";
+'use client';
 
-import TextField from "@/components/text-field/text-field.component";
-import { Button } from "@/components/button";
-import { ButtonHierarchy } from "@/enum/button-hierarchy.enum";
-import React from "react";
-import useLoginUserForm from "@/app/(public)/login/components/use-login-user-form.hook";
+import TextField from '@/components/text-field/text-field.component';
+import { Button } from '@/components/button';
+import { ButtonHierarchy } from '@/enum/button-hierarchy.enum';
+import React from 'react';
+import useLoginUserForm from '@/app/(public)/login/components/use-login-user-form.hook';
+import { useRouter } from 'next/navigation';
 
 export default function LoginUserForm() {
   const { loginUserInputsModel, submitForm } = useLoginUserForm();
+
+  const { push } = useRouter();
 
   return (
     <form className="flex flex-col gap-8" onSubmit={submitForm}>
@@ -15,7 +18,11 @@ export default function LoginUserForm() {
         <TextField key={index} {...fields} />
       ))}
 
-      <Button.Wrapper type={"submit"} hierarchy={ButtonHierarchy.Filled}>
+      <Button.Wrapper
+        type={'submit'}
+        hierarchy={ButtonHierarchy.Filled}
+        onClick={() => push('/')}
+      >
         Login
       </Button.Wrapper>
     </form>
