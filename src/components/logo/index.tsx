4d585c1e1ef555cd7ee66f746372ce68a62e67db
leonardorossi <08.leo.rossi@gmail.com>
2024-03-15 23:21:17 -0300

    feat: Componente para o logo do sistema

---
+++ src/components/logo/index.tsx
@@ -0,0 +1,7 @@
+import LogoSmall from "@/components/logo/logo-small.component";
+import LogoMedium from "@/components/logo/logo-medium.component";
+
+export const Logo = {
+  Small: LogoSmall,
+  Medium: LogoMedium
+}
---
+++ src/components/logo/logo-medium.component.tsx
@@ -0,0 +1,5 @@
+export default function LogoMedium() {
+  return (
+    <h1 className={"text-4xl text-gray-50 text-center"}>Project Name</h1>
+  )
+}
