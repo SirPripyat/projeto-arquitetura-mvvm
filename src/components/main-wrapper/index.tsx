a652d9f24977a5d2c886dd80ed8ec559b812fb6a
leonardorossi <08.leo.rossi@gmail.com>
2024-03-15 23:21:31 -0300

    feat: Componente principal para o main-wrapper

---
+++ src/components/main-wrapper/index.tsx
@@ -0,0 +1,7 @@
+import MainWrapperAuthenticated from '@/components/main-wrapper/main-wrapper-authenticated.component';
+import MainWrapperPublic from '@/components/main-wrapper/main-wrapper-public.component';
+
+export const MainWrapper = {
+  Authenticated: MainWrapperAuthenticated,
+  Public: MainWrapperPublic,
+};
